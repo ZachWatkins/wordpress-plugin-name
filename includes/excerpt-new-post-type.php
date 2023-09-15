978322ae8ec1153f3a3bd7acda2cc388a278b479
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-15 16:25:55 -0500

    Remove unused option call

--- includes/excerpt-new-post-type.php
+++ includes/excerpt-new-post-type.php
@@ -8,7 +8,7 @@
 
 ?>
 <div class="new-post-type fields excerpt">
-	<p><span class="title">Unique Text Field Setting:</span> <?php echo esc_html( get_option( 'unique_text_field' ) ); ?></p>
+	<p><span class="title">Post Fields from Advanced Custom Fields</p>
 	<p><span class="title">Field 1:</span> <?php the_field( 'post_field_1' ); ?></p>
 	<p><span class="title">Field 2:</span> <?php the_field( 'post_field_2' ); ?></p>
 	<p><span class="title">Field 3:</span> <?php the_field( 'post_field_3' ); ?></p>
