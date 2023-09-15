caf3ee45d960b8992c8da854e7dc5676a31efd72
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-15 16:25:15 -0500

    Add plugin namespace prefix

--- .wp-env/database.php
+++ .wp-env/database.php
@@ -6,11 +6,11 @@
 global $wpdb;
 
 // Set option "seeded" to "true".
-$wordpress_theme_seeded = get_option( 'seeded' );
+$wordpress_theme_seeded = get_option( 'wordpress_plugin_name_seeded' );
 if ( $wordpress_theme_seeded ) {
 	echo "Database already seeded.\n";
 	return;
 }
 
-update_option( 'seeded', true );
+update_option( 'wordpress_plugin_name_seeded', true );
 echo "Database seeded.\n";
