5a87d1158ad637bb79a2016b9c3d7a7c4f970d12
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-15 12:40:53 -0500

    Add wp-env folder

---
+++ .wp-env/database.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Seed the database.
+ */
+
+global $wpdb;
+
+// Set option "seeded" to "true".
+$wordpress_theme_seeded = get_option( 'seeded' );
+if ( $wordpress_theme_seeded ) {
+	echo "Database already seeded.\n";
+	return;
+}
+
+update_option( 'seeded', true );
+echo "Database seeded.\n";
