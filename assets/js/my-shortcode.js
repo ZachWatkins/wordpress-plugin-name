4251d481b8db26d76cc74d1fc32940ee3bb57d2f
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-18 19:11:28 -0500

    Improve JS component values

--- assets/js/my-shortcode.js
+++ assets/js/my-shortcode.js
@@ -6,16 +6,19 @@
  */
 ( function () {
 	let countValue = 0;
+
 	const counter = document.createElement( 'span' );
+	counter.classList.add( 'my-shortcode-counter' );
+
 	const button = document.createElement( 'button' );
+	button.innerHTML = 'Click to increase counter';
 	button.addEventListener( 'click', function () {
 		countValue += 1;
 		counter.innerHTML = countValue;
 	} );
 
 	const container = document.getElementById( 'my-shortcode' );
-	container.innerHTML +=
-		' Your shortcode is loaded. Click the button to increase the counter.';
+	container.innerHTML += ' Your shortcode is loaded.';
 	container.appendChild( button );
 	container.appendChild( counter );
 } )();
