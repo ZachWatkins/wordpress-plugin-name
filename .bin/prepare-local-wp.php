23dacfd5c8beec158e4c4436920f98b71d5525b6
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-20 21:15:31 -0500

    Generate a default WordPress theme to allow the site to load

--- .bin/prepare-local-wp.php
+++ .bin/prepare-local-wp.php
@@ -53,9 +53,8 @@ require dirname( __FILE__, 5 ) . \'/vendor/wordpress/sqlite-database-integration
 '
 );
 
-echo "Success: SQLite database driver added.\n";
-echo "+ ./wordpress/wp-content/db.php\n";
 echo "+ ./wordpress/wp-content/mu-plugins/sqlite-database-integration/plugin.php\n";
+echo "+ ./wordpress/wp-content/db.php\n";
 
 // Create a symbolic link for the plugin directory.
 if ( file_exists( dirname( __DIR__ ) . '/wordpress/wp-content/mu-plugins/wordpress-plugin-name' ) ) {
@@ -71,7 +70,6 @@ symlink(
 	dirname( __DIR__ ) . '/wordpress/wp-content/mu-plugins/wordpress-plugin-name'
 );
 
-echo "Success: Symbolic link created.\n";
 echo "+ ./wordpress/wp-content/mu-plugins/wordpress-plugin-name -> ./\n";
 
 if ( file_exists( dirname( __DIR__ ) . '/wordpress/wp-config-sample.php' ) ) {
@@ -85,5 +83,78 @@ if ( file_exists( dirname( __DIR__ ) . '/wordpress/wp-config-sample.php' ) ) {
 	);
 }
 
-echo "Success: wp-config.php created.\n";
 echo "+ ./wordpress/wp-config.php\n";
+
+if ( file_exists( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme' ) ) {
+	if ( file_exists( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme/style.css' ) ) {
+		unlink( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme/style.css' );
+	}
+
+	if ( file_exists( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme/index.php' ) ) {
+		unlink( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme/index.php' );
+	}
+
+	rmdir( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme' );
+}
+
+mkdir( dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme', 0777, true );
+
+file_put_contents(
+	dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme/style.css',
+	'/*
+!
+Theme Name: No Theme
+Description: Minimal default theme to provide a local development environment.
+*/'
+);
+
+echo "+ ./wordpress/wp-content/themes/no-theme/style.css\n";
+
+file_put_contents(
+	dirname( __DIR__ ) . '/wordpress/wp-content/themes/no-theme/index.php',
+	<<<HEREDOC
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<title><?php wp_title( ' | ', true, 'right' ); ?></title>
+<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+<?php wp_head(); ?>
+</head>
+<body>
+<h1><?php bloginfo( 'name' ); ?></h1>
+<h2><?php bloginfo( 'description' ); ?></h2>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<h3><?php the_title(); ?></h3>
+
+<?php the_content(); ?>
+<?php wp_link_pages(); ?>
+<?php edit_post_link(); ?>
+
+<?php endwhile; ?>
+
+<?php
+if ( get_next_posts_link() ) {
+next_posts_link();
+}
+?>
+<?php
+if ( get_previous_posts_link() ) {
+previous_posts_link();
+}
+?>
+
+<?php else: ?>
+
+<p>No posts found. :(</p>
+
+<?php endif; ?>
+<?php wp_footer(); ?>
+</body>
+</html>
+HEREDOC
+);
+
+echo "+ ./wordpress/wp-content/themes/no-theme/index.php\n";
