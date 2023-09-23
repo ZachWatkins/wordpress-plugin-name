90be7ed5b4940d62c6fe7f3c7f0883b233e0e28e
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-23 14:53:57 -0500

    Improve the installer and its feedback

--- .bin/install-wsl-no-admin.sh
+++ .bin/install-wsl-no-admin.sh
@@ -8,22 +8,32 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 PHP_VERSION="8.1.2"
-NODE_VERSION="18.18.0"
+NODE_VERSION="18"
 
-# Install NodeJS.
+if ! command -v curl &> /dev/null
+then
+    ${SUDO} apt-get update
+    ${SUDO} apt-get -y install curl
+fi
+
+INSTALL_NODE=false
 if ! command -v node &> /dev/null
 then
-    echo "NodeJS v${NODE_VERSION} could not be found. Installing..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    ${SUDO} chmod +x $HOME/.nvm/nvm.sh
-    ${SUDO} ${NVM_DIR}/nvm.sh install $NODE_VERSION
-    ${SUDO} ${NVM_DIR}/nvm.sh alias default $NODE_VERSION
+    INSTALL_NODE=true
+else
+    NODE_VERSION_INSTALLED=`node -v | cut -d "v" -f 2 | cut -d "." -f 1`
+    if [ "$NODE_VERSION_INSTALLED" != "$NODE_VERSION" ]; then
+        INSTALL_NODE=true
+    fi
+fi
+
+if [ "$INSTALL_NODE" = true ]; then
+    ${SUDO} apt-get update
+    ${SUDO} curl -sL https://deb.nodesource.com/setup_${NODE_VERSION}.x | ${SUDO} -E bash -
+    ${SUDO} apt install nodejs -y
     echo "NodeJS v${NODE_VERSION} installed."
 fi
 
-# Install PHP and Composer.
 INSTALL_PHP=false
 if ! command -v php &> /dev/null
 then
@@ -36,7 +46,6 @@ else
 fi
 
 if [ "$INSTALL_PHP" = true ]; then
-    echo "PHP v${PHP_VERSION} could not be found. Installing..."
     ${SUDO} apt-get update
     ${SUDO} apt-get -y install lsb-release ca-certificates curl
     ${SUDO} curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
@@ -58,41 +67,65 @@ if [ "$INSTALL_PHP" = true ]; then
     ${SUDO} apt install "php${PHP_VERSION}-sqlite3" --no-install-recommends
     ${SUDO} apt install "php${PHP_VERSION}-mysql" --no-install-recommends
     ${SUDO} apt install "php${PHP_VERSION}-bcmath" --no-install-recommends
+    echo "PHP v${PHP_VERSION} installed."
+fi
+
+INSTALL_COMPOSER=false
+if ! command -v composer &> /dev/null
+then
+    INSTALL_COMPOSER=true
+fi
+
+if [ "$INSTALL_COMPOSER" = true ]; then
+    ${SUDO} apt-get update
     ${SUDO} apt install composer --no-install-recommends
-    echo "PHP installed."
+    echo "Composer installed"
 fi
 
 if [ ! -d "node_modules" ]; then
     npm install
+    echo "+ node_modules/"
 fi
 
 if [ ! -d "vendor" ]; then
     composer install
+    echo "+ vendor/"
 fi
 
 if [ ! -d "wordpress/wp-content" ]; then
     mkdir wordpress/wp-content
+    echo "+ wordpress/wp-content/"
 fi
 
 if [ ! -d "wordpress/wp-content/mu-plugins" ]; then
     mkdir wordpress/wp-content/mu-plugins
+    echo "+ wordpress/wp-content/mu-plugins/"
 fi
 
 if [ ! -d "wordpress/wp-content/plugins" ]; then
     mkdir wordpress/wp-content/plugins
+    echo "+ wordpress/wp-content/plugins/"
 fi
 
 if [ ! -f "wordpress/wp-config.php" ]; then
     cp wordpress/wp-config-sample.php wordpress/wp-config.php
+    echo "+ wordpress/wp-config.php"
 fi
 
 if [ ! -d "wordpress/wp-content/plugins/wordpress-plugin-name" ]; then
-    ln -s "$(pwd)" wordpress/wp-content/plugins/wordpress-plugin-name
+    ${SUDO} ln -s "$(pwd)" wordpress/wp-content/plugins/wordpress-plugin-name
+    echo "+ wordpress/wp-content/plugins/wordpress-plugin-name -> $(pwd)"
+fi
+
+if [ ! -f ".bin/.tmp/twentytwentythree.zip" ]; then
+    ${SUDO} curl -L https://downloads.wordpress.org/theme/twentytwentythree.1.2.zip -o .bin/.tmp/twentytwentythree.zip
+    echo "+ .bin/.tmp/twentytwentythree.zip"
 fi
 
 if [ ! -d "wordpress/wp-content/themes/twentytwentythree" ]; then
-    curl -L https://downloads.wordpress.org/theme/twentytwentythree.1.2.zip -o .bin/.tmp/twentytwentythree.zip
+    ${SUDO} apt-get -y install unzip
     unzip .bin/.tmp/twentytwentythree.zip -d wordpress/wp-content/themes
+    echo "+ wordpress/wp-content/themes/twentytwentythree"
 fi
 
 if [ ! -f "wordpress/wp-content/db.php" ]; then
@@ -100,10 +133,12 @@ if [ ! -f "wordpress/wp-content/db.php" ]; then
     CONTENTS=${CONTENTS//"{SQLITE_IMPLEMENTATION_FOLDER_PATH}"/"$(pwd)/vendor/wordpress/sqlite-database-integration"}
     CONTENTS=${CONTENTS//"{SQLITE_PLUGIN}"/"sqlite-database-integration/load.php"}
     echo "$CONTENTS" > wordpress/wp-content/db.php
+    echo "+ wordpress/wp-content/db.php"
 fi
 
 if [ ! -d "wordpress/wp-content/mu-plugins/sqlite-database-integration" ]; then
     ln -s "$(pwd)/vendor/wordpress/sqlite-database-integration" wordpress/wp-content/mu-plugins/sqlite-database-integration
+    echo "+ wordpress/wp-content/mu-plugins/sqlite-database-integration -> $(pwd)/vendor/wordpress/sqlite-database-integration"
 fi
 
-echo "Installation finished. You must restart your terminal session before changes will take effect."
+echo "Installation finished. You must restart your terminal session before using newly installed command line applications."
