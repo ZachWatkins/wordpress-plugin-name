78b90591ad79499b7c4158fe9c1cc94f919bdfe5
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-25 22:12:11 -0500

    Add a new post using the seeder

--- .wp-env/database.php
+++ .wp-env/database.php
@@ -14,6 +14,16 @@ if ( $wordpress_theme_seeded ) {
 	echo "Database already seeded.\n";
 	return;
 }
-
 update_option( 'wordpress_plugin_name_seeded', true );
 echo "Database seeded.\n";
+
+// Add a post to the database with the title "Hello, WordPress!" and include the shortcode "my-shortcode" in the content.
+$wpdb->insert(
+	$wpdb->posts,
+	array(
+		'post_title'   => 'Hello, WordPress!',
+		'post_content' => '[my-shortcode]',
+		'post_status'  => 'publish',
+		'post_type'    => 'post',
+	)
+);
