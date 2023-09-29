141c3e6eb29732361893d3647ce3f3efb7578d55
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-29 12:10:48 -0500

    Move custom post type registration and rendering logic to the root plugin file and write it in a more functional way for easier onboarding

--- wordpress-plugin-name.php
+++ wordpress-plugin-name.php
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/src/assets.php';
-require __DIR__ . '/src/shortcode.php';
+require __DIR__ . '/src/demo.php';
 
 const VERSION   = '1.0.0';
 const ROOT_FILE = __FILE__;
@@ -38,6 +38,83 @@ const ROOT_FILE = __FILE__;
 enqueue_js( 'src/assets/js/public.js' );
 enqueue_css( 'src/assets/css/public.css' );
 
-register_shortcode( 'my-shortcode', 'src/views/my-shortcode.php' );
-new NewPostType();
+add_shortcode(
+	'my-shortcode',
+	function ( $atts ) {
+		$atts = shortcode_atts( array( 'id' => 'my-shortcode' ), $atts );
+		ob_start();
+		include __DIR__ . '/src/views/my-shortcode.php';
+		$output = ob_get_clean();
+		return wp_kses( $output, 'post' );
+	}
+);
+
+add_action(
+	'init',
+	function () {
+		// https://developer.wordpress.org/reference/functions/register_post_type/.
+		register_post_type(
+			'custom_post_type',
+			array(
+				'label'            => 'Custom Post Types',
+				'labels'           => array(
+					'name'          => __( 'Custom Post Types', 'wordpress-plugin-name-textdomain' ),
+					'singular_name' => __( 'Custom Post Type', 'wordpress-plugin-name-textdomain' ),
+				),
+				'description'      => 'Custom post type to demonstrate how to add them to a website using a plugin.',
+				'public'           => true,
+				'show_in_rest'     => true,
+				'menu_position'    => 20,
+				'menu_icon'        => 'dashicons-portfolio',
+				'supports'         => array(
+					'title',
+					'editor',
+					'author',
+					'custom-fields',
+					'page-attributes',
+					'excerpt',
+				),
+				'has_archive'      => true,
+				'rewrite'          => array( 'slug' => 'custom-posts' ),
+				'delete_with_user' => false,
+			),
+		);
+
+		enqueue_js( 'src/assets/js/single-new-post-type.js', fn () => is_singular( 'custom_post_type' ) );
+		enqueue_css( 'src/assets/css/single-new-post-type.css', fn () => is_singular( 'custom_post_type' ) );
+		enqueue_css( 'src/assets/css/archive-new-post-type.css', fn () => is_post_type_archive( 'custom_post_type' ) );
+
+		add_filter(
+			'the_content',
+			function ( $content ) {
+				if ( get_post_type() === 'custom_post_type' && is_singular( 'custom_post_type' ) ) {
+					ob_start();
+					include __DIR__ . '/src/views/content-new-post-type.php';
+					$template = ob_get_clean();
+					$content .= $template;
+				}
+
+				return $content;
+			}
+		);
+
+		add_filter(
+			'the_excerpt',
+			function ( $excerpt ) {
+				if ( get_post_type() === 'custom_post_type' && ! is_singular( 'custom_post_type' ) ) {
+					ob_start();
+					include __DIR__ . '/src/views/excerpt-new-post-type.php';
+					$template = ob_get_clean();
+					$excerpt .= $template;
+				}
+
+				return $excerpt;
+			}
+		);
+	}
+);
+
+register_activation_hook( __FILE__, 'flush_rewrite_rules' );
+register_deactivation_hook( __FILE__, fn () => unregister_post_type( 'new_post_type' ) && flush_rewrite_rules() );
+
 new \ThoughtfulWeb\SettingsPageWP\Page();
