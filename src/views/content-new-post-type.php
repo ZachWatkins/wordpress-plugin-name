70ada49e4ca424ba376ab1c5993c063d2af75878
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-30 13:50:23 -0500

    Simplify files

--- src/views/content-new-post-type.php
+++ src/views/content-new-post-type.php
@@ -7,9 +7,7 @@
  */
 
 ?>
-<div class="new-post-type fields content">
+<div class="custom-post-type fields content">
 	<p><span class="title">Post Meta Fields</p>
 	<p><span class="title">Field 1:</span> <?php get_post_meta( get_the_ID(), 'post_field_1', true ); ?></p>
-	<p><span class="title">Field 2:</span> <?php get_post_meta( get_the_ID(), 'post_field_2', true ); ?></p>
-	<p><span class="title">Field 3:</span> <?php get_post_meta( get_the_ID(), 'post_field_3', true ); ?></p>
 </div>
