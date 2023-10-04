9fd809a61f20bebcc200c8ab6887883c59c284b6
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-10-03 21:10:17 -0500

    Search within files for presence of text before laerting user it will be replaced

--- .bin/template.js
+++ .bin/template.js
@@ -32,14 +32,12 @@ const ARGS = {
 	NEW_PLUGIN_REPOSITORY: null,
 	NEW_PLUGIN_COPYRIGHT: 'Zachary K. Watkins ' + new Date().getFullYear(),
 	FILES: [
-		'.bin/install-wsl-no-admin.sh',
-		'.config/phpcs.xml.dist',
+		'.config/.phpcs.xml.dist',
 		'.wp-env/database.php',
 		'.wp-env/database.sql',
-		'assets/',
+		'.wp-env/schema.sql',
+		'.wp-env/seeds.sql',
 		'common/',
-		'config/',
-		'includes/',
 		'src/',
 		'test/',
 		'.wp-env.json',
@@ -47,7 +45,7 @@ const ARGS = {
 		'composer.lock',
 		'package.json',
 		'package-lock.json',
-		'wordpress-plugin-name.php',
+		'index.php',
 	],
 	DIRNAME: path.dirname( import.meta.url.replace( 'file://', '' ) ),
 	ROOT: path.resolve(
@@ -69,21 +67,91 @@ rl.question( "What is your plugin's name? ", ( answer ) => {
 			"What is your plugin's repository URL? https://github.com/zachwatkins/",
 			( answer ) => {
 				ARGS.NEW_PLUGIN_REPOSITORY = `https://github.com/zachwatkins/${ answer }`;
+				const filtered = searchFilesForChanges( ARGS );
 				rl.write( 'The following changes will be made:\n' );
-				rl.write( getChangeExplanation( ARGS ) );
+				rl.write( getChangeExplanation( filtered ) );
 				rl.question( 'Proceed? [Y/n] ', ( answer ) => {
 					if ( answer.toLowerCase() !== 'y' ) {
 						rl.close();
 						process.exit( 0 );
 						return;
 					}
-					handle( ARGS );
+					handle( filtered );
 					rl.close();
 				} );
 			}
 		);
 	} );
 } );
+
+/**
+ * Filter the arguments by searching through the file list and returning only the arguments that were found.
+ *
+ * @param {ARGS} args - Arguments provided by the user and request context.
+ * @return {object} The filtered arguments.
+ */
+function searchFilesForChanges( args ) {
+	const filteredArgs = {};
+	for ( const key in args ) {
+		if ( 0 > key.indexOf( 'CURRENT' ) ) {
+			filteredArgs[ key ] = args[ key ];
+			continue;
+		}
+		const value = args[ key ];
+		if (
+			typeof value === 'string' &&
+			searchFilesForValue( value, args.FILES )
+		) {
+			filteredArgs[ key ] = value;
+			continue;
+		}
+		if ( typeof value === 'object' ) {
+			filteredArgs[ key ] = {};
+			for ( const subkey in value ) {
+				if ( typeof filteredArgs[ key ][ subkey ] !== 'undefined' ) {
+					// Only search for each value once.
+					continue;
+				}
+				const subvalue = value[ subkey ];
+				if (
+					typeof subvalue === 'string' &&
+					searchFilesForValue( subvalue, args.FILES )
+				) {
+					filteredArgs[ key ][ subkey ] = subvalue;
+				}
+			}
+		}
+	}
+	return filteredArgs;
+}
+
+/**
+ * Search through the given files for the given value and return true if the value is found in any file.
+ *
+ * @param {any} value
+ * @param {string[]} files
+ * @return {boolean} True if the value is found in any file, false otherwise.
+ */
+function searchFilesForValue( value, files ) {
+	for ( let i = 0; i < files.length; i++ ) {
+		const file = files[ i ];
+		if ( fs.lstatSync( file ).isDirectory() ) {
+			const found = searchFilesForValue(
+				value,
+				fs.readdirSync( file ).map( ( f ) => path.join( file, f ) )
+			);
+			if ( found ) {
+				return true;
+			}
+		} else {
+			const content = fs.readFileSync( file, 'utf8' );
+			if ( 0 <= content.indexOf( value ) ) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
 
 /**
  * Get a message explaining the changes they are about to make to the template repository.
@@ -131,32 +199,11 @@ function getChangeExplanation( args ) {
  * @returns {void}
  */
 function handle( args ) {
-	const currentValues = {
-		name: args.CURRENT_PLUGIN_NAME.name,
-		slug: args.CURRENT_PLUGIN_NAME.slug,
-		prefix: args.CURRENT_PLUGIN_NAME.prefix,
-		namespace: args.CURRENT_PLUGIN_NAME.namespace,
-		constant: args.CURRENT_PLUGIN_NAME.constant,
-		textdomain: args.CURRENT_PLUGIN_NAME.textdomain,
-		directoryName: args.CURRENT_PLUGIN_NAME.directoryName,
-		rootPluginFileName: args.CURRENT_PLUGIN_NAME.rootPluginFileName,
-		description: args.CURRENT_PLUGIN_DESCRIPTION,
-		repository: args.CURRENT_PLUGIN_REPOSITORY,
-		copyright: args.CURRENT_PLUGIN_COPYRIGHT,
-	};
-	const newValues = {
-		name: args.NEW_PLUGIN_NAME.name,
-		slug: args.NEW_PLUGIN_NAME.slug,
-		prefix: args.NEW_PLUGIN_NAME.prefix,
-		namespace: args.NEW_PLUGIN_NAME.namespace,
-		constant: args.NEW_PLUGIN_NAME.constant,
-		textdomain: args.NEW_PLUGIN_NAME.textdomain,
-		directoryName: args.NEW_PLUGIN_NAME.directoryName,
-		rootPluginFileName: args.NEW_PLUGIN_NAME.rootPluginFileName,
-		description: args.NEW_PLUGIN_DESCRIPTION,
-		repository: args.NEW_PLUGIN_REPOSITORY,
-		copyright: args.NEW_PLUGIN_COPYRIGHT,
-	};
+	const currentValues = args.CURRENT_PLUGIN_NAME;
+	const newValues = {};
+	for ( const key in currentValues ) {
+		newValues[ key ] = args.NEW_PLUGIN_NAME[ key ];
+	}
 	// Replace text within files.
 	for ( let i = 0; i < args.FILES.length; i++ ) {
 		replace(
@@ -164,27 +211,6 @@ function handle( args ) {
 			currentValues,
 			newValues,
 			args.VERBOSE
-		);
-	}
-	// Rename files.
-	const currentFileName = path.join(
-		args.ROOT,
-		currentValues.rootPluginFileName
-	);
-	if ( fs.existsSync( currentFileName ) ) {
-		fs.renameSync(
-			currentFileName,
-			path.join( args.ROOT, newValues.rootPluginFileName )
-		);
-	}
-	const currentDirectoryName = path.join(
-		args.ROOT,
-		currentValues.directoryName
-	);
-	if ( fs.existsSync( currentDirectoryName ) ) {
-		fs.renameSync(
-			currentDirectoryName,
-			path.join( args.ROOT, newValues.directoryName )
 		);
 	}
 }
@@ -261,37 +287,24 @@ function replace( fileOrDirectory, oldValues, newValues, verbose ) {
  * @class
  * @param {string} [name="WordPress Plugin Name"] - Plugin name shown to users in the dashboard.
  * @property {string} name - Plugin name shown to users in the dashboard.
- * @property {string} slug - Slug name of the plugin. Used in the plugin's directory name, root PHP file name, textdomain, repository name, and more. Must be lowercase with hyphens instead of spaces and all other special characters and numbers removed.
+ * @property {string} slug - Slug name of the plugin. Used in the plugin's directory name, textdomain, repository name, and more. Must be lowercase with hyphens instead of spaces and all other special characters and numbers removed.
  * @property {string} prefix - Database and global function name prefix used in all PHP files declared by the plugin. Must be lowercase with underscores instead of spaces and hyphens. Here we assume that the plugin's prefix will be the same as its lowercase name with underscores instead of spaces and hyphens and no other special characters or numbers.
  * @property {string} namespace - PHP class namespace used in all PHP classes declared by the plugin. Replace spaces with underscores and remove all other special characters and numbers.
  * @property {string} constant - Constant name prefix used in all global PHP constants declared by the plugin. Must be uppercase with underscores instead of spaces. Here we assume that the plugin's constant prefix will be the same as its prefix in uppercase.
  * @property {string} textdomain - Name of the plugin's textdomain. Used to provide text translations in the dashboard and website. Must be lowercase with hyphens instead of spaces, and here we assume that the plugin's textdomain will be the same as its slug plus "-textdomain".
- * @property {string} directoryName - Name of the plugin's root directory. Must be equal to the plugin slug.
- * @property {string} rootPluginFileName - Name of the plugin's root PHP file. Must be the file which contains the plugin's registration metadata.
  */
 function PluginName( name ) {
-	this.name = name || 'WordPress Plugin Name';
+	name = name?.trim() || 'WordPress Plugin Name';
+	this.name = name;
 	this.slug = name
-		? name
-				.toLowerCase()
-				.replace( /[^a-z\s]/g, '' )
-				.replace( /\s+/g, '-' )
-		: 'wordpress-plugin-name';
-	this.prefix = name
-		? this.slug.replace( /-/g, '_' )
-		: 'wordpress_plugin_name';
+		.toLowerCase()
+		.replace( /[^a-z\s]/g, '' )
+		.replace( /\s+/g, '-' );
+	this.prefix = this.slug.replace( /-/g, '_' );
 	this.namespace = name
-		? name
-				.replace( /\s+/g, '_' )
-				.replace( /-/g, '_' )
-				.replace( /[^a-z_]/gi, '' )
-		: 'WordPress_Plugin_Name';
-	this.constant = name ? this.prefix.toUpperCase() : 'WORDPRESS_PLUGIN_NAME';
-	this.textdomain = name
-		? this.slug + '-textdomain'
-		: 'wordpress-plugin-name-textdomain';
-	this.directoryName = name ? this.slug : 'wordpress-plugin-name';
-	this.rootPluginFileName = name
-		? this.slug + '.php'
-		: 'wordpress-plugin-name.php';
+		.replace( /\s+/g, '_' )
+		.replace( /-/g, '_' )
+		.replace( /[^a-z_]/gi, '' );
+	this.constant = this.prefix.toUpperCase();
+	this.textdomain = this.slug + '-textdomain';
 }
