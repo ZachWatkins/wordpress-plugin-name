d7e3f3bbbbc910a78b794a7ee36287088ebcee18
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-10-07 15:28:25 -0500

    Load the library index file for reusable functions

--- index.php
+++ index.php
@@ -29,12 +29,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Do not directly request this file in your browser.' );
 }
 
+require 'library/index.php';
+
 const PLUGIN_FILE   = __FILE__;
 const PLUGIN_KEY    = 'wordpress-plugin-name';
 const POST_TYPE_KEY = 'custom_post_type';
 define( 'WordPress_Plugin_Name\PLUGIN_URL', plugin_dir_url( __FILE__ ) . 'src' );
 
-require 'src/functions.php';
 require 'src/shortcode.php';
 require 'src/post-type.php';
 require 'src/demo.php';
