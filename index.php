69b4ff19679a95cddacda2a501820ac43a23232f
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-10-18 13:29:00 -0500

    Correct new const value

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 const PLUGIN_FILE   = __FILE__;
-const PLUGIN_DIR    = __FILE__ . '/';
+const PLUGIN_DIR    = __DIR__ . '/';
 const PLUGIN_KEY    = 'wordpress-plugin-name';
 const POST_TYPE_KEY = 'custom_post_type';
 define( 'WordPress_Plugin_Name\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
