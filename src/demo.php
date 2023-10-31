0bc744e12faee4232354cd44d8797775341b672b
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-10-30 20:52:32 -0500

    Improve separation of concerns in demo file

--- src/demo.php
+++ src/demo.php
@@ -2,10 +2,7 @@
 /**
  * Make changes to the site to set up demo content, etc.
  *
- * @package   WordPress_Plugin_Name
- * @copyright Zachary K. Watkins 2023
- * @author    Zachary K. Watkins <zwatkins.it@gmail.com
- * @license   GPL-2.0-or-later
+ * @package WordPress_Plugin_Name
  */
 
 namespace WordPress_Plugin_Name;
@@ -14,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'We\'re sorry, but you can not directly access this file.' );
 }
 
-if ( 'local' === wp_get_environment_type() && strpos( get_site_url(), 'localhost' ) !== false && file_exists( __DIR__ . '/local.php' ) ) {
+if ( 'local' !== wp_get_environment_type() || strpos( get_site_url(), 'localhost' ) !== false ) {
 	return;
 }
 
@@ -22,16 +19,44 @@ if ( 'local' === wp_get_environment_type() && strpos( get_site_url(), 'localhost
 add_action(
 	'init',
 	function () {
-		// We want to avoid running this function if the plugin is not being developed in isolation from a custom theme.
-		$current_theme      = wp_get_theme();
-		$wp_themes          = array( 'Twenty Twenty', 'Twenty Twenty-One', 'Twenty Twenty-Two', 'Twenty Twenty-Three', 'Twenty Twenty-Four' );
-		$current_theme_name = $current_theme->get( 'Name' );
-		if ( ! in_array( $current_theme_name, $wp_themes, true ) ) {
+		if ( ! using_default_theme() ) {
 			return;
 		}
-		// Create the demo post.
-		$demo_post_values = array(
-			'post_title'   => 'Plugin Demo Page',
+
+		$demo_post_id = get_or_create_demo_post();
+
+		if ( get_option( 'page_on_front' ) !== $demo_post_id ) {
+			update_option( 'show_on_front', 'page' );
+			update_option( 'page_on_front', $demo_post_id );
+		}
+	}
+);
+
+/** Get the Post ID for the demo post, and create it if it doesn't exist. */
+function get_or_create_demo_post(): int {
+
+	$post_title = 'Plugin Demo Page';
+	$query      = new \WP_Query(
+		array(
+			'post_title'     => $post_title,
+			'post_type'      => 'page',
+			'post_status'    => 'any',
+			'posts_per_page' => 1,
+			'fields'         => 'ids',
+		)
+	);
+
+	if ( $query->posts ) {
+		return $query->posts[0];
+	}
+
+	// Create the demo post.
+	return wp_insert_post(
+		array(
+			'post_title'   => $post_title,
+			'post_type'    => 'page',
+			'post_status'  => 'publish',
+			'post_author'  => 1,
 			'post_content' => '<!-- wp:paragraph -->
 <p>This is a post for the custom post type provided by the plugin. You can see output below for the shortcode it provides called "[my-shortcode]".</p>
 <!-- /wp:paragraph -->
@@ -39,23 +64,14 @@ add_action(
 <!-- wp:shortcode -->
 [my-shortcode]
 <!-- /wp:shortcode -->',
-			'post_status'  => 'publish',
-			'post_type'    => POST_TYPE_KEY,
-			'post_author'  => 1,
-		);
-		$demo_post        = new \WP_Query(
-			array(
-				'post_type'      => POST_TYPE_KEY,
-				'post_status'    => 'any',
-				'posts_per_page' => 1,
-				'fields'         => 'ids',
-				'post_title'     => $demo_post_values['post_title'],
-			)
-		);
-		if ( empty( $demo_post->posts ) ) {
-			$demo_post_id = wp_insert_post( $demo_post_values );
-			update_option( 'show_on_front', 'page' );
-			update_option( 'page_on_front', $demo_post_id );
-		}
-	}
-);
+		)
+	);
+}
+
+/** Detect use of default theme in current site. */
+function using_default_theme(): bool {
+	$current_theme      = wp_get_theme();
+	$wp_themes          = array( 'Twenty Twenty', 'Twenty Twenty-One', 'Twenty Twenty-Two', 'Twenty Twenty-Three', 'Twenty Twenty-Four' );
+	$current_theme_name = $current_theme->get( 'Name' );
+	return in_array( $current_theme_name, $wp_themes, true );
+}
